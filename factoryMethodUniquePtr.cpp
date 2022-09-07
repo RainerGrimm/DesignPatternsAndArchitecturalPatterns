03d5c7dcf27ab7c906836eb3de21fa9847faf06d
RainerGrimm <rainer@grimm-jaud.de>
2022-09-07 12:12:08 +0200

    factorMethod examples added

--- factoryMethodUniquePtr.cpp
+++ factoryMethodUniquePtr.cpp
@@ -4,38 +4,43 @@
 // Product
 class Window{ 
  public: 
-    virtual std::unique_ptr<Window> clone() = 0; 
+    virtual std::unique_ptr<Window> create() { 
+        std::cout << "Create Window" << '\n';
+        return std::make_unique<Window>();
+    } 
 };
 
 // Concrete Products 
 class DefaultWindow: public Window {
-    std::unique_ptr<Window> clone() override { 
-        std::cout << "DefaultWindow" << '\n';
+    std::unique_ptr<Window> create() override { 
+        std::cout << "Create DefaultWindow" << '\n';
         return std::make_unique<DefaultWindow>();
     } 
 };
 
 class FancyWindow: public Window {
-    std::unique_ptr<Window> clone() override { 
-        std::cout << "FancyWindow" << '\n';
+    std::unique_ptr<Window> create() override { 
+        std::cout << "Create FancyWindow" << '\n';
         return std::make_unique<FancyWindow>();
     } 
 };
 
 // Concrete Creator or Client
-auto getNewWindow(std::unique_ptr<Window>& oldWindow) { 
-    return oldWindow->clone();
+auto createWindow(std::unique_ptr<Window>& oldWindow) { 
+    return oldWindow->create();
 }
   
 int main() {
 
     std::cout << '\n';
 
+    std::unique_ptr<Window> window = std::make_unique<Window>();
     std::unique_ptr<Window> defaultWindow = std::make_unique<DefaultWindow>();
     std::unique_ptr<Window> fancyWindow = std::make_unique<FancyWindow>();
   
-    const auto defaultWindow1 = getNewWindow(defaultWindow);
-    const auto fancyWindow1 = getNewWindow(fancyWindow);
+    const auto window1 = createWindow(window);
+    const auto defaultWindow1 = createWindow(defaultWindow);
+    const auto fancyWindow1 = createWindow(fancyWindow);
 
     std::cout << '\n';
   
