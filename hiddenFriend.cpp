7f5ba81e97df89e2bcdf4837dfddb9e08e04a8e5
RainerGrimm <rainer@grimm-jaud.de>
2022-10-24 14:28:29 +0200

    Fixed examples.

--- hiddenFriend.cpp
+++ hiddenFriend.cpp
@@ -1,33 +1,38 @@
+// hiddenFriend.cpp
+
 #include <iostream>
 
 class MyDistance{
-    public:
-      MyDistance(double i):m(i){}
+ public:
+    explicit MyDistance(double i):m(i){}
 
-      friend MyDistance operator +(const MyDistance& a, const MyDistance& b){
+    friend MyDistance operator +(const MyDistance& a, const MyDistance& b){
         return MyDistance(a.m + b.m);
-      }
-      friend MyDistance operator -(const MyDistance& a, const MyDistance& b);
+    }
+    
+    friend MyDistance operator -(const MyDistance& a, const MyDistance& b){
+        return MyDistance(a.m - b.m);
+    }
 
-      friend std::ostream& operator<< (std::ostream &out, const MyDistance& myDist){
+    friend std::ostream& operator<< (std::ostream &out, const MyDistance& myDist){
         out << myDist.m << " m";
-         return out;
-      }
-    private:
-      double m;
+        return out;
+    }
+    
+ private:
+    double m;
 
 };
 
-MyDistance operator -(const MyDistance& a, const MyDistance& b){
-    return MyDistance(a.m - b.m);
-}
 
 int main() {
 
-    std::cout << MyDistance(5.5) + 5.5 << "\n";
-    std::cout << 5.5 + MyDistance(5.5) << "\n";
+    std::cout << '\n';
 
-    std::cout << MyDistance(5.5) - 5.5 << "\n";
-    std::cout << 5.5 - MyDistance(5.5) << "\n";
+    std::cout << "MyDistance(5.5) + MyDistance(5.5): " << MyDistance(5.5) + MyDistance(5.5) << '\n';
+
+     std::cout << "MyDistance(5.5) - MyDistance(5.5): " << MyDistance(5.5) - MyDistance(5.5) << '\n';
+
+    std::cout << '\n';
 
 }
--- solution/hiddenFriendExplicit.cpp
+++ solution/hiddenFriendExplicit.cpp
@@ -1,46 +1,41 @@
 #include <iostream>
 
 class MyDistance{
-    public:
-      explicit MyDistance(double i):m(i){}
+ public:
+    explicit MyDistance(double i):m(i){}
 
-      friend MyDistance operator +(const MyDistance& a, const MyDistance& b){
+    friend MyDistance operator +(const MyDistance& a, const MyDistance& b){
         return MyDistance(a.m + b.m);
-      }
+    }
 
-      friend MyDistance operator +(const MyDistance& a, double b){
+    friend MyDistance operator +(const MyDistance& a, double b){
         return MyDistance(a.m + b);
-      }
+    }
 
-      friend MyDistance operator +(double a, const MyDistance& b){
+    friend MyDistance operator +(double a, const MyDistance& b){
         return MyDistance(a + b.m);
-      }
+    }
 
+    friend MyDistance operator -(const MyDistance& a, const MyDistance& b){
+        return MyDistance(a.m - b.m);
+    }
 
-      friend MyDistance operator -(const MyDistance& a, const MyDistance& b);
-      friend MyDistance operator -(const MyDistance& a, double b);
-      friend MyDistance operator -(double a, const MyDistance& b);
+    friend MyDistance operator -(const MyDistance& a, double b){
+        return MyDistance(a.m - b);
+    }
 
-      friend std::ostream& operator<< (std::ostream &out, const MyDistance& myDist){
+    friend MyDistance operator -(double a, const MyDistance& b){
+        return MyDistance(a - b.m);
+    }
+
+    friend std::ostream& operator<< (std::ostream &out, const MyDistance& myDist){
         out << myDist.m << " m";
-         return out;
-      }
-    private:
-      double m;
+        return out;
+    }
+ private:
+    double m;
 
 };
-
-MyDistance operator -(const MyDistance& a, const MyDistance& b){
-    return MyDistance(a.m - b.m);
-}
-
-MyDistance operator -(const MyDistance& a, double b){
-    return MyDistance(a.m - b);
-}
-
-MyDistance operator -(double a, const MyDistance& b){
-    return MyDistance(a - b.m);
-}
 
 int main() {
 
